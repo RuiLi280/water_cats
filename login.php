e9750cba4bb9e3383cfbe9c2293c3b8b41310efa
RUI LI <lr_280@qq.com>
2018-02-24 16:08:50 -0800

    login

--- login.php
+++ login.php
@@ -1,12 +1,40 @@
-<?php
-    $email = filter_input(INPUT_POST, "email");
-    $password = filter_input(INPUT_POST,passwd);
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Login</title>
+</head>
 
-    try {
-        $con = new PDO("mysql:host=localhost;dbname=water_cats","water_cats","sesame");
-        $con.setAttribute(PDO::ATTR_ERROR, PDO::ERRMODE_EXCEPTION);
+<body>
+    <?php
+        $email = filter_input(INPUT_POST, "email");
+        $password = filter_input(INPUT_POST,"passwd");
 
-        $query = "SELECT * FROM person WHERE email = :email";
-        
-    }
-?>
+        try {
+            $con = new PDO("mysql:host=localhost;dbname=water_cats","water_cats","sesame");
+            $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+            $query = "SELECT * FROM person WHERE email = :email";
+            $ps = $con->prepare($query);
+
+            $ps->execute(array(':email' => $email));
+            $data = $ps->fetchAll(PDO::FETCH_ASSOC);
+
+            if(count($data) > 0) {
+                if($password == $data[0]["password"]) {
+                    header("Location: http://localhost/board.html");
+                    exit();
+                }
+            }
+            print "<p>Incorrect email or password.</p>";
+            print "<button onclick='location.href=\"login.html\"'>return</button>";
+        }
+        catch(PDOException $ex){
+            echo 'ERROR: '.$ex->getMessage();
+        }
+        catch(Exception $ex){
+            echo 'ERROR: '.$ex->getMessage();
+        }
+    ?>
+</body>
+</html>
