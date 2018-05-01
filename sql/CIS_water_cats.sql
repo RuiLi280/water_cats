fa96b3ea2877999bcf38865cc12087f1ece4df16
Michelle Song <michelle.song@sjsu.edu>
2018-05-01 12:29:43 -0700

    change db name cis_water_cats -> watercats

---
+++ sql/CIS_water_cats.sql
@@ -0,0 +1,18 @@
+DROP DATABASE IF EXISTS watercats;
+CREATE DATABASE watercats;
+USE watercats;
+
+CREATE TABLE person (
+	personID int NOT NULL,
+    firstName varchar(50) NOT NULL,
+    lastName varchar(50) NOT NULL,
+    email varchar(100) NOT NULL
+);
+
+INSERT INTO person (personID, firstName, lastName, email) VALUES
+(100, 'Jason', 'Kelly', 'jkelly@example.com'),
+(101, 'Arthur', 'Patterson', 'apatterson@example.com'),
+(102, 'Janice', 'Jenkinson', 'jjenkinson@example.com'),
+(103, 'Adam', 'Alexander', 'aalexander@example.com'),
+(104, 'Robert', 'Nelson', 'rnelson@example.com'),
+(105, 'Ron', 'Mak', 'rmak@example.com');
---
+++ sql/cis_water_cats_dump.sql
@@ -0,0 +1,53 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 23, 2018 at 11:25 PM
+-- Server version: 10.1.26-MariaDB
+-- PHP Version: 7.1.8
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `watercats`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `person`
+--
+
+CREATE TABLE `person` (
+  `personID` int(11) NOT NULL,
+  `firstName` varchar(50) NOT NULL,
+  `lastName` varchar(50) NOT NULL,
+  `email` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `person`
+--
+
+INSERT INTO `person` (`personID`, `firstName`, `lastName`, `email`) VALUES
+(100, 'Jason', 'Kelly', 'jkelly@example.com'),
+(101, 'Arthur', 'Patterson', 'apatterson@example.com'),
+(102, 'Janice', 'Jenkinson', 'jjenkinson@example.com'),
+(103, 'Adam', 'Alexander', 'aalexander@example.com'),
+(104, 'Robert', 'Nelson', 'rnelson@example.com'),
+(105, 'Ron', 'Mak', 'rmak@example.com');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
